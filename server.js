5d66b10eae31f7a963282231fca3228270be546d
gopss936 <158065113+gopss936@users.noreply.github.com>
2024-03-23 17:58:27 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const routes= require('./routes/index');
+require('dotenv').config();
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/ecommerce',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDb'))
+.catch(() => console.error('Error connecting to MongoDb',err));
+app.use(express.json());
+app.use(morgan('dev'));
+app.use('/api',routes);
+
+app.use(express.urlencoded({extended: true}));
+const PORT=process.env.PORT || 5001;
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
